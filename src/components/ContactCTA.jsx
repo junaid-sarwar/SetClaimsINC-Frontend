6e137da023e06d56ffae3c38eb8b32665e0dfcde
junaid-sarwar <junaidsarwariqbal@gmail.com>
2026-01-19 16:21:24 +0500

    fixing email thing

--- src/components/ContactCTA.jsx
+++ src/components/ContactCTA.jsx
@@ -27,7 +27,7 @@ const handleSubmit = async (e) => {
   setStatus(null);
 
   try {
-    const response = await fetch(`${apiURL}/api/contact`, {
+    const response = await fetch(`https://setclaimsinc-backend.onrender.com/api/contact`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
